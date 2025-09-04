6b6b37731b2c9450da62229e35e15c4a3924083c
stevenreed <reedsteven397@gmail.com>
2025-09-04 14:56:42 -0500

    Add memory_continuity module for narrative simulation engine

---
+++ memory_continuity.py
@@ -0,0 +1,15 @@
+class MemoryContinuity:
+    """
+    Maintains continuity of possession-level trends, momentum, and emotion.
+    """
+    def __init__(self):
+        self.memory = []
+
+    def update(self, possession_state, outcome, tags):
+        self.memory.append({
+            'possession_state': possession_state.copy(),
+            'outcome': outcome.copy(),
+            'tags': tags[:]
+        })
+
+memory_continuity = MemoryContinuity()
