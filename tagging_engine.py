701e58ede71d1898dc9f8604c11f0db974606147
stevenreed <reedsteven397@gmail.com>
2025-09-04 14:56:13 -0500

    Add tagging_engine module for narrative simulation engine

---
+++ tagging_engine.py
@@ -0,0 +1,11 @@
+def apply_tags(play_call, outcome, possession_state):
+    """
+    Applies strategic, emotional, and predictive tags to a play.
+    Returns a list of tags.
+    """
+    tags = []
+    if possession_state.get('down') == 3 and outcome.get('yards', 0) >= possession_state.get('distance', 10):
+        tags.append("3rd down conversion")
+    if outcome.get('turnover'):
+        tags.append("turnover")
+    return tags
