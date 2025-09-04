24832287f6efd96806c4999f71b76a6451be5f46
stevenreed <reedsteven397@gmail.com>
2025-09-04 14:55:52 -0500

    Add clock_manager module for narrative simulation engine

---
+++ clock_manager.py
@@ -0,0 +1,7 @@
+def update_clock(clock, play_type, outcome):
+    """
+    Updates the game clock based on play type and outcome.
+    """
+    if play_type == 'run':
+        return clock - 40
+    return clock - 30
