01217bd647ff76dad5cdf5749d3a6512efe99f39
stevenreed <reedsteven397@gmail.com>
2025-09-05 17:35:37 -0500

    Add example script for config helpers

---
+++ example_usage.py
@@ -0,0 +1,9 @@
+from config import get_s3_settings, get_mongo_settings, get_redis_settings
+
+def print_settings():
+    print("S3:", get_s3_settings())
+    print("Mongo:", get_mongo_settings())
+    print("Redis:", get_redis_settings())
+
+if __name__ == "__main__":
+    print_settings()
